5cdd7ae727fff0e990a7b7368aecafc8783aa3b0
Chalana Lasith <chalanalasith0@gmail.com>
2025-07-10 23:23:21 +0530

    test

--- App/Views/surf-ui/home.php
+++ App/Views/surf-ui/home.php
@@ -7,7 +7,7 @@
     <img src="assets/img/hero-bg.jpg" alt="" data-aos="fade-in">
 
     <div class="container">
-      <h2 data-aos="fade-up" data-aos-delay="100">Unlock New Opportunities<br>by Learning Japanese.</h2>
+      <h2 data-aos="fade-up" data-aos-delay="100">Unlock New Opportunitie<br>by Learning Japanese.</h2>
       <p data-aos="fade-up" data-aos-delay="200">Build Your Future with the Power of Language.</p>
       <div class="d-flex mt-4" data-aos="fade-up" data-aos-delay="300">
         <a href="courses.php" class="btn-get-started">Register Now</a>
